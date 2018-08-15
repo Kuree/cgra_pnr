6bc6961ba9b880a1028c531e67b1582e59e5e211
Keyi <keyi@stanford.edu>
2018-08-14 20:04:40 -0700

    fix travis build

--- .travis.sh
+++ .travis.sh
@@ -4,8 +4,6 @@ declare -a design_files=("conv_1_2_mapped" "conv_2_1_mapped" "conv_3_1_mapped"
                          "conv_bw_mapped" "onebit_bool_mapped" "pointwise_mapped")
 
 
-mkdir design_files
-cd design_files
 echo "Downloading CGRA info"
 wget https://github.com/StanfordAHA/CGRAGenerator/raw/master/hardware/generator_z/top/cgra_info.txt.dev
 cgra="cgra_info.txt.dev"
@@ -16,8 +14,8 @@ do
     rm -f ${file}.json
     wget "https://github.com/StanfordAHA/CGRAGenerator/raw/master/bitstream/bsbuilder/testdir/examples/${file}.json"
     echo "Running PnR tools on ${file}"
-    ../scripts/pnr_flow.sh $cgra $file.json
+    ./scripts/pnr_flow.sh $cgra $file.json
     echo "Running PnR tools on ${file} -no-reg-fold"
-    ../scripts/pnr_flow.sh -no-reg-fold $cgra $file.json
+    ./scripts/pnr_flow.sh -no-reg-fold $cgra $file.json
 done
 
