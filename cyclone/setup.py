19c799323fdd53fb8788ec9ce226a4312371c5e5
Keyi Zhang <keyi@stanford.edu>
2020-10-13 00:07:28 -0700

    bump version

--- cyclone/setup.py
+++ cyclone/setup.py
@@ -72,7 +72,7 @@ with open(os.path.join(current_directory, 'README.rst')) as f:
 
 setup(
     name='pycyclone',
-    version='0.3.25',
+    version='0.3.26',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Fast CGRA Routing',
