8717b8ec789cb09268b5ccce8f73243b510b25fc
Keyi Zhang <keyi@stanford.edu>
2021-04-25 14:34:17 -0700

    add support for py3.9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,16 +5,16 @@ if [[ "$OS" == "linux" ]]; then
     if [[ "$BUILD_WHEEL" == true ]]; then
         docker cp ~/.pypirc manylinux:/home/
         docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && for PYBIN in cp36 cp37; do /opt/python/${PYBIN}-${PYBIN}m/bin/python setup.py bdist_wheel; done'
-        # python 3.8 has different name now
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && /opt/python/cp38-cp38/bin/python setup.py bdist_wheel'
+        # python 3.8+ has different names now
+        docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && for PYBIN in cp38 cp39; do /opt/python/${PYBIN}-${PYBIN}/bin/python setup.py bdist_wheel; done'
         docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && for WHEEL in dist/*.whl; do auditwheel repair "${WHEEL}"; done'
         docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && twine upload --config-file /home/.pypirc --skip-existing wheelhouse/*'
         # upload the src
         docker exec -i manylinux bash -c 'cd /cgra_pnr/thunder && python setup.py sdist && twine upload --config-file /home/.pypirc --skip-existing dist/*.gz'
 
         docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && for PYBIN in cp36 cp37; do /opt/python/${PYBIN}-${PYBIN}m/bin/python setup.py bdist_wheel; done'
-        # python 3.8 has different name now
-        docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && /opt/python/cp38-cp38/bin/python setup.py bdist_wheel'
+        # python 3.8+ has different names now
+        docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && for PYBIN in cp38 cp39; do /opt/python/${PYBIN}-${PYBIN}/bin/python setup.py bdist_wheel; done'
         docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && for WHEEL in dist/*.whl; do auditwheel repair "${WHEEL}"; done'
         docker exec -i manylinux bash -c 'cd /cgra_pnr/cyclone && twine upload --config-file /home/.pypirc --skip-existing wheelhouse/*'
         # upload the src
