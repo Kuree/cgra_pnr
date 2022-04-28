53fa4d60047415cb07bcedf1422309da56056977
Keyi Zhang <keyi@stanford.edu>
2022-04-28 11:55:51 -0700

    bump version

--- thunder/setup.py
+++ thunder/setup.py
@@ -72,7 +72,7 @@ with open(os.path.join(current_directory, 'README.rst')) as f:
 
 setup(
     name='pythunder',
-    version='0.4.1',
+    version='0.4.2',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Fast CGRA Placement',
